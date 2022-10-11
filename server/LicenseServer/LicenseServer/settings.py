82c5be3ccbf6bd73fb3bd6b96c90e8f78ed7c74a
Daniel Walz <developer@foleysfinest.com>
2022-10-12 00:42:59 +0200

    Fixed settings

--- server/LicenseServer/LicenseServer/settings.py
+++ server/LicenseServer/LicenseServer/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
