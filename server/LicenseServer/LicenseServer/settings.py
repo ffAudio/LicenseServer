7fc691a6a55dbd1cf423911f75373f2439cb1df2
Daniel Walz <developer@foleysfinest.com>
2022-10-12 15:16:52 +0200

    Connect to postgresql

--- server/LicenseServer/LicenseServer/settings.py
+++ server/LicenseServer/LicenseServer/settings.py
@@ -74,12 +74,8 @@ WSGI_APPLICATION = 'LicenseServer.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
