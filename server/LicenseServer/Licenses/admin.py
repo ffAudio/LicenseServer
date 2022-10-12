66db4495c596af403196e435bddf3da8c7eeefe5
Daniel Walz <developer@foleysfinest.com>
2022-10-12 23:36:20 +0200

    Added admin panels for models

--- server/LicenseServer/Licenses/admin.py
+++ server/LicenseServer/Licenses/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Product, Version, Customer, License, Activation
+
+admin.site.register(Product)
+admin.site.register(Version)
+admin.site.register(Customer)
+admin.site.register(License)
+admin.site.register(Activation)
