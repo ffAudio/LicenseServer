4f9f6e0970102b734f254225936cd5a436cc8799
Daniel Walz <developer@foleysfinest.com>
2022-10-25 00:14:38 +0200

    Fixed string function

--- server/LicenseServer/Licenses/models.py
+++ server/LicenseServer/Licenses/models.py
@@ -52,7 +52,7 @@ class Version(models.Model):
     created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.product.name + "-" + self.name + "(" + str(id) + ")"
+        return self.product.name + "-" + self.version_string + "(" + str(id) + ")"
 
 class Customer(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
