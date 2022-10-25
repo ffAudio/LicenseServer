0c26d50994d975750bcac694e3dc586c9e24e69c
Daniel Walz <developer@foleysfinest.com>
2022-10-25 22:30:51 +0200

    Added accounts views

--- server/LicenseServer/LicenseServer/urls.py
+++ server/LicenseServer/LicenseServer/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include('Licenses.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('admin/',   admin.site.urls),
 ]
